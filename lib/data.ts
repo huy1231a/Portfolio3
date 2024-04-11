656424d35c0ac62c23cab198ec66b1a9858dd260
huy1231a <taolahuy2k0@gmail.com>
2024-04-11 16:47:26 +0700

    add new status

--- lib/data.ts
+++ lib/data.ts
@@ -35,28 +35,27 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: 'Graduated bootcamp',
-    location: 'Miami, FL',
+    title: 'Hutech University',
+    location: 'Ho Chi Minh',
     description:
-      'I graduated after 6 months of studying. I immediately found a job as a front-end developer.',
+      'I graduated after 4 years of studying. I immediately found a job as a front-end developer.',
     icon: React.createElement(LuGraduationCap),
-    date: '2019',
+    date: '2018',
   },
   {
     title: 'Front-End Developer',
-    location: 'Orlando, FL',
-    description:
-      'I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.',
+    location: 'Ho Chi Minh',
+    description: 'I worked as a part-time front-end developer for 1 years.',
     icon: React.createElement(CgWorkAlt),
-    date: '2019 - 2021',
+    date: '2021 - 2022',
   },
   {
-    title: 'Full-Stack Developer',
-    location: 'Houston, TX',
+    title: 'Front-End Developer',
+    location: 'Ho Chi Minh',
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I'm front-end developer working. My stack includes React, Next.js, TypeScript, Redux-saga, .... I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: '2021 - present',
+    date: '2022 - present',
   },
 ] as const
 
