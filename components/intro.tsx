b62b7743cc42358368302fd6af38231e19644983
nQHuy2 <67337070+huy1231a@users.noreply.github.com>
2024-06-04 02:24:58 +0000

    fix bug

--- components/intro.tsx
+++ components/intro.tsx
@@ -61,7 +61,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}>
         <span className='font-bold'>Hello, I'm Huy.</span> I'm a{' '}
         <span className='font-bold'>front-end developer</span> with{' '}
-        <span className='font-bold'>3 years</span> of experience. I enjoy
+        <span className='font-bold'>2.5 years</span> of experience. I enjoy
         building <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
       </motion.h1>
